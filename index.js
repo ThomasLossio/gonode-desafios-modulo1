f1f3fb218998f960695e954ca07546a28390822f
ThomasNeo <thomaslossio@hotmail.com>
2019-01-19 21:14:11 -0300

    Desafio completo

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+const checkMiddleware = (req, res, next) => {
+  const { age } = req.query
+
+  if (age > 0) {
+    next()
+  } else {
+    return res.redirect('/')
+  }
+}
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+app.get('/', (req, res) => {
+  return res.render('form')
+})
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+
+  if (age >= 18) {
+    return res.redirect(`/major?age=${age}`)
+  } else {
+    return res.redirect(`/minor?age=${age}`)
+  }
+})
+
+app.get('/major', checkMiddleware, (req, res) => {
+  const { age } = req.query
+
+  return res.render('major', { age })
+})
+
+app.get('/minor', checkMiddleware, (req, res) => {
+  const { age } = req.query
+
+  return res.render('minor', { age })
+})
+
+app.listen(3000)
